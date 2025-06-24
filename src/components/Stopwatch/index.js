ab2e96a3265b7e9206fc37189713b9bf63eab74d
rakesh <rakeshsagartfi21@gmail.com>
2025-06-24 12:05:29 +0000

    Added CSS

--- src/components/Stopwatch/index.js
+++ src/components/Stopwatch/index.js
@@ -6,49 +6,56 @@ import './index.css'
 class Stopwatch extends Component {
   state = {
     timeElapsedInSeconds: 0,
-    isTimerRunning: 0
+    isTimerRunning: 0,
   }
 
-  renderSeconds = () =>{
-    const {timeElapsedInSeconds} = this.state 
+  renderSeconds = () => {
+    const {timeElapsedInSeconds} = this.state
 
     const seconds = Math.floor(timeElapsedInSeconds % 60)
 
-    if (seconds < 10){
+    if (seconds < 10) {
       return `0${seconds}`
     }
     return seconds
   }
 
+  renderMinutes = () => {
+    const {timeElapsedInSeconds} = this.state
 
-  renderMinutes = () =>{
-    const {timeElapsedInSeconds} = this.state 
-
-    const minutes = Math.floor(timeElapsedInSeconds/60)
-    if (minutes < 10){
+    const minutes = Math.floor(timeElapsedInSeconds / 60)
+    if (minutes < 10) {
       return `0${minutes}`
     }
     return minutes
   }
+
   render() {
     const time = `${this.renderMinutes()}:${this.renderSeconds()}`
     return (
-      <div>
-        <div>
-          <h1> Stopwatch </h1>
-          <div>
-            <div>
+      <div className="app-container">
+        <div className="stopwatch-container">
+          <h1 className="stopwatch"> Stopwatch </h1>
+          <div className="timer-container">
+            <div className="timer">
               <img
                 src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png"
                 alt="stopwatch"
+                className="timer-image"
               />
-              <p> Timer </p>
+              <p className="heading"> Timer </p>
             </div>
-            <h1>{time}</h1>
-            <div>
-              <button type="button">Start</button>
-              <button type="button">Stop</button>
-              <button type="button">Reset</button>
+            <h1 className="stopwatch-timer">{time}</h1>
+            <div className="timer-buttons">
+              <button type="button" className="start-button button">
+                Start
+              </button>
+              <button type="button" className="stop-button button">
+                Stop
+              </button>
+              <button type="button" className="reset-button button">
+                Reset
+              </button>
             </div>
           </div>
         </div>
